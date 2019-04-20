506b3a5624078be6ff2c822bba1a11d5f41990ff
GregoryUrban <turknben@gmail.com>
2019-04-20 12:46:39 -0400

    Get working, fetchsmurfsstart and success working - updated index, app, actions, reducer

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getSmurfs } from '../actions';
+import { connect } from 'react-redux';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,6 +9,11 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,9 +21,31 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+
+        {this.props.smurfs.map(smurf => <h2>{smurf.name}</h2>)}
       </div>
     );
   }
 }
+// export default App;
 
-export default App;
+function mapStateToProps(state) {
+  console.log(state);
+  return{
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs,
+    addingSmurf: state.addingSmurf,
+    updatingSmurf: state.updatingSmurf,
+    deletingSmurf: state.deletingSmurf,
+    error: state.error
+  }
+}
+
+
+export default connect(
+  mapStateToProps,
+  {
+    getSmurfs
+  }
+)(App);
+
